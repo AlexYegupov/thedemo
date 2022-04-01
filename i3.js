e9c12f834a26b4b615089506b0995520d9a8b4ee
Alex_Y <yegupov@gmail.com>
2022-04-01 15:24:53 +0300

    rewrote i3 to async/await

--- i3.js
+++ i3.js
@@ -1,77 +1,62 @@
-//let google = require('googleapis');
+// working via googleapis with table https://docs.google.com/spreadsheets/d/1-SIrWNLeDRTDHdjqcsChrkWNYKzzlar223kK83Ovmew/edit#gid=0
+
 const { google } = require('googleapis');
+const sheets = google.sheets('v4');
+
 let privatekey = require("./privatekey.json");
-
-// configure a JWT auth client
-let jwtClient = new google.auth.JWT(
-  privatekey.client_email,
-  null,
-  privatekey.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets',
-   'https://www.googleapis.com/auth/drive',
-   'https://www.googleapis.com/auth/calendar']
-);
-
-//authenticate request
-jwtClient.authorize(function (err, tokens) {
-  if (err) {
-    Console.log(err);
-    return;
-  } else {
-    console.log("ok");
-  }
-});
-
-
-
-
-//Google Sheets API
-let sheets = google.sheets('v4');
-
 const spreadsheetId = '1-SIrWNLeDRTDHdjqcsChrkWNYKzzlar223kK83Ovmew';
 
-/*
- * sheets.spreadsheets.values.get({
- *   auth: jwtClient,
- *   spreadsheetId,
- *   range: 'Sheet1!A5:B10'
- * }, function (err, response) {
- *   if (err) {
- *     console.log('The API returned an error: ' + err);
- *   } else {
- *     console.log('Movie list from Google Sheets:');
- *     for (let row of response.data.values) {
- *       console.log('%s :: %s', row[0], row[1]);
- *     }
- *   }
- * })
- *  */
+(async () => {
+  let jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive',
+     'https://www.googleapis.com/auth/calendar']
+  );
 
-// https://docs.google.com/spreadsheets/d/1-SIrWNLeDRTDHdjqcsChrkWNYKzzlar223kK83Ovmew/edit#gid=0
-c
-sheets.spreadsheets.values.update({
-  auth: jwtClient,
-  spreadsheetId,
-  range: 'Sheet1!B3:B4',
-  valueInputOption:'RAW',
-  resource: {
-    values: [[new Date()], [new Date()]],
+  //authenticate request
+  try {
+    const au = await jwtClient.authorize()
+    console.log('authorized')
+    //console.log(au)
+  } catch(error) {
+    throw error
   }
 
-  //majorDimensions: "ROWS"
-  //range: 'Sheet1!B3:B3',
-  //,
-  //? resource,
-  /* data: [{
-   *   range: 'Sheet1!A3:A3',
-   *   majorDimension: "ROWS",
-   *   values: [[new Date()]]
-   * }] */
-}, (err, result) => {
-  if (err) {
-    throw err;
-    //console.log(err);
-  } else {
-    console.log('%d cells updated.', result.updatedCells);
+  // update
+  try {
+    const result = await sheets.spreadsheets.values.update(
+      {
+        auth: jwtClient,
+        spreadsheetId,
+        range: 'Sheet1!B3:C4',
+        valueInputOption:'RAW',
+        resource: {
+          values: [
+            ['B3', new Date()],
+            ['C3', 'C4']
+          ],
+        }
+      }
+    )
+    console.log(`Updated: ${result.data.updatedCells} cells`)
+  } catch (error) {
+    console.error('Update error:', error)
   }
-});
+
+  // get
+  try {
+    const getResult = await sheets.spreadsheets.values.get({
+      auth: jwtClient,
+      spreadsheetId,
+      range: 'Sheet1!B2:C4'
+    })
+    console.log('Got values:', getResult.data.values)
+  } catch (error) {
+    console.error('Get error:', error)
+  }
+
+})()
+
